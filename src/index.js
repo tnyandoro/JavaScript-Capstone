bb0746591e7065e00f88bac6b2ca8dbf6d97662f
Tendai Nyandoro <tendai1@hotmail.com>
2021-08-09 22:58:24 +0200

    Set up babel

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@ const $ = require('jquery');
 
 $('body').css({ 'background-color': '#eee' });
 $('h1#title')
-.css({'font-size': '80px', 'text-align': 'center', 'margin-top': '100px'});
-.text('Hello, world');
+  .css({ 'font-size': '80px', 'text-align': 'center', 'margin-top': '100px' })
+  .text('Hello, world');
---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const path = require('path');
+const { WebpackManifestPlugin } = require('webpack-manifest-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const options = {};
+const mode = process.env.NODE_ENV === 'development';
+// Temporary workaround for browserslist a bug thats being patched in the near future
+const target = process.env.NODE_ENV === 'production' ? 'browserslist' : 'web';
+
+module.exports = {
+  mode: 'development',
+
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+    new WebpackManifestPlugin(options),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            cacheDirectory: true,
+            plugins: ['@babel/plugin-transform-runtime'],
+          },
+        },
+      },
+    ],
+  },
+  target: 'web',
+  devtool: 'source-map',
+
+  devServer: {
+    contentBase: './dist',
+  },
+};
