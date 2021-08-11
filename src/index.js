c90f53937ee6465931ff5ec68104041f6fe61e2c
Tendai Nyandoro <tendai1@hotmail.com>
2021-08-11 10:13:58 +0200

    Homepage

--- src/index.js
+++ src/index.js
@@ -1,58 +1,85 @@
-const createElements = () => {
-  const row = document.getElementById('grid-container');
-  // row.classList = 'grid-container';
+// window.addEventListener('load', () =>  {
+const foodList = document.getElementById('grid-container');
+foodList.classList = 'food-list';
 
-  const column = document.createElement('div');
-  column.classList = 'column';
-  const card = document.createElement('div');
-  card.classList = 'card';
+const foodItem = document.createElement('div');
+foodItem.classList = 'column';
+const card = document.createElement('div');
+card.classList = 'card';
 
-  const foodImage = document.createElement('img');
-  foodImage.classList = 'food-image';
-  foodImage.src = '../images/food1.jpg';
-  foodImage.alt = 'food-item';
+const foodImage = document.createElement('img');
+foodImage.classList = 'food-image';
+foodImage.src = '../images/food1.jpg';
+foodImage.alt = 'food-item';
 
-  const cardBody = document.createElement('div');
-  cardBody.classList = 'card-body';
+const cardBody = document.createElement('div');
+cardBody.classList = 'card-body';
 
-  const cardHeader = document.createElement('div');
-  cardHeader.classList = 'card-header';
+const cardHeader = document.createElement('div');
+cardHeader.classList = 'card-header';
 
-  const foodName = document.createElement('h4');
-  foodName.classList = '';
+const foodName = document.createElement('h4');
+foodName.classList = '';
 
-  const likeBtn = document.createElement('div');
-  likeBtn.classList = 'like-btn';
+const likeBtn = document.createElement('div');
+likeBtn.classList = 'like-btn';
 
-  const btn = document.createElement('button');
-  btn.classList = 'like-food';
+const iconBtn = document.createElement('button');
+const iconHeart = document.createElement('i');
+iconHeart.classList = 'far fa-heart';
+iconBtn.classList = 'like-food';
 
-  const iconHeart = document.createElement('i');
-  iconHeart.classList = 'icon-heart';
+const likes = document.createElement('p');
+likes.classList = 'likes';
+likes.textContent = 'likes';
 
-  const likes = document.createElement('p');
-  likes.classList = 'likes';
+const cardBtns = document.createElement('div');
+cardBtns.classList = 'card-buttons';
 
-  const cardBtns = document.createElement('div');
-  cardBtns.classList = 'card-buttons';
+const commentsBtn = document.createElement('button');
+commentsBtn.classList = 'btn-primary';
+commentsBtn.textContent = 'Comments';
 
-  const commentsBtn = document.createElement('button');
-  commentsBtn.classList = 'btn-primary';
+document.body.append(foodList);
+foodList.appendChild(card);
+foodList.appendChild(foodImage);
+foodList.appendChild(cardBody);
+foodList.appendChild(cardHeader);
+foodList.appendChild(foodName);
+foodList.appendChild(likeBtn);
+foodList.appendChild(iconBtn);
+iconBtn.appendChild(iconHeart);
+foodList.appendChild(likes);
+foodList.appendChild(cardBtns);
+foodList.appendChild(commentsBtn);
+// };
 
-  const reservationsBtn = document.createElement('button');
-  reservationsBtn.classList = 'btn-primary';
+// const renderFoods = () => {
+//   // clear items HTML
+//   foodList.innerHTML = '';
 
-  row.appendChild(column);
-  row.appendChild(card);
-  row.appendChild(foodImage);
-  row.appendChild(cardBody);
-  row.appendChild(cardHeader);
-  row.appendChild(foodName);
-  row.appendChild(likeBtn);
-  row.appendChild(btn);
-  row.appendChild(iconHeart);
-  row.appendChild(likes);
-  row.appendChild(cardBtns);
-  row.appendChild(commentsBtn);
-  row.appendChild(reservationsBtn);
-};
+//   foodList.insertAdjacentHTML(
+//     'beforeend',
+//     `<div class="column">
+//           <div class="card">
+//             <img src="../images/food1.jpg" alt="" class="food-image" />
+//             <div class="card-body">
+//               <div class="card-header">
+//                 <h4>Potato Salad</h4>
+//                 <div class="like-button">
+//                   <button data-id="like-meal" class ="like-food">
+//                     <i class="far fa-heart"></i>
+//                   </button>
+//                   <p class="likes"><span data-id="likes">0</span> Likes</p>
+//                 </div>
+//               </div>
+//               <div class="card-buttons">
+//                 <button data-id="comments" class="btn-primary">Comments</button><br><br>
+//                 <button data-id="reservations" class="btn-primary">Reservations</button>
+//               </div>
+//             </div>
+//           </div>
+//         </div>`,
+//   );
+// };
+// renderFoods();
