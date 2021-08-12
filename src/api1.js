1f1ad906a8460bc585cfb2251798c1a66162ff03
uwadonat <uwadonat@gmail.com>
2021-08-12 10:10:31 +0200

    Fetch data from MealDB api

---
+++ src/api1.js
@@ -0,0 +1,18 @@
+import cardList from "./components/cardList";
+import commentPopup from "./components/commentPopup";
+
+class Meal {
+    static async getList(category) {
+        const response = await fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?c=${category}`);
+        const list = await response.json();
+        cardList(list.meals);   
+;    }
+
+    static async getItem(id) {
+       const response = await fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`);
+       const mealDetails = await response.json();
+       commentPopup(mealDetails.meals[0]);
+    }
+}
+
+export default Meal;
--- src/index.js
+++ src/index.js
@@ -1,27 +1,8 @@
-import _ from 'lodash';
 import './style.css';
+import './bootstrap.css';
+
 import mainpage from './mainpage.js';
-import Comments from './api.js';
-import commentPage from './comment.js';
-import * as API from './api.js';
+
+
 
 mainpage();
-
-commentPage();
-
-const comment = new Comments();
-
-async function startCommenting() {
-  await comment.createComment('Donat');
-}
-startCommenting();
-
-document.getElementById('addCommentbtn').addEventListener('click', () => {
-  comment.displayScores();
-}, false);
-
-document.getElementById('addCommentbtn').addEventListener('click', () => {
-  comment.addComment();
-}, false);
-
-document.getElementById('comment-btn').addEventListener('click', commentPage);
