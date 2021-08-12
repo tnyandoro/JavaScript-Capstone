494cfb0e21058d9e99652f0ee3cb29e6b5b55f97
uwadonat <uwadonat@gmail.com>
2021-08-12 10:20:02 +0200

    Add cardlist file to display meal list

---
+++ src/components/cardList.js
@@ -0,0 +1,21 @@
+import './cardList.css';
+import card from './card';
+import Meal from '../api1';
+
+const cardList = (list) => {
+    const listContainer = document.getElementById("meal-items");
+    list.forEach((meal) => {
+        listContainer.innerHTML += card(meal);
+    });
+
+    const commentBtns = document.querySelectorAll('.comment-btn');
+    
+    commentBtns.forEach((btn) => {
+        btn.addEventListener('click', () => { 
+            Meal.getItem(btn.dataset.itemid);
+
+        });
+    });
+}
+
+export default cardList;
