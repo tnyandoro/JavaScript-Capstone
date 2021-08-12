9c3d4331cf4c966b6d6278539296993405320772
Tendai Nyandoro <tendai1@hotmail.com>
2021-08-12 18:55:02 +0200

    fix the feedback

--- src/components/card.js
+++ src/components/card.js
@@ -5,7 +5,7 @@ const card = (meal, likeItem, category) => `
         <img src=${meal.strMealThumb}>
         <div class="desc">
             <h3>${meal.strMeal}</h3>
-            <div><span class="likes">${likeItem === undefined ? 0 : likeItem.likes}</span> likes <button type="button" class="like-btn" data-itemid=${meal.idMeal} data-cat=${category}><i class="fa fa-heart"></i></button></div>
+            <div class="feedback"><span class="likes">${likeItem === undefined ? 0 : likeItem.likes}</span> likes <button type="button" class="like-btn" data-itemid=${meal.idMeal} data-cat=${category}><i class="fa fa-heart"></i></button></div>
             <button type="button" class="comment-btn" data-itemId=${meal.idMeal}>Comment</button>
         </div>
     </div>
